1157250b3d3abe8c8dd4194f6ec8bec87274fd6b
Saeid Akbari <saeid9063@gmail.com>
2018-01-03 18:52:47 +0330

    Here is router with ease

---
+++ scanner.go
@@ -0,0 +1,81 @@
+package Scanner
+
+import (
+	"database/sql"
+	"reflect"
+	"encoding/json"
+)
+
+// Costume scanner type
+type Scanner struct {
+	*sql.DB
+}
+
+// New return the costume scanner instance
+func New(db *sql.DB) *Scanner {
+	return &Scanner{db}
+}
+
+// row2mapInterface get rows and fields name and return array of interface
+func row2mapInterface(rows *sql.Rows, fields []string) (resultsMap map[string]interface{}, err error) {
+	resultsMap = make(map[string]interface{}, len(fields))
+	scanResultContainers := make([]interface{}, len(fields))
+	for i := 0; i < len(fields); i++ {
+		var scanResultContainer interface{}
+		scanResultContainers[i] = &scanResultContainer
+	}
+	if err := rows.Scan(scanResultContainers...); err != nil {
+		return nil, err
+	}
+
+	for ii, key := range fields {
+		resultsMap[key] = reflect.Indirect(reflect.ValueOf(scanResultContainers[ii])).Interface()
+	}
+	return
+}
+
+// rows2Interfaces return the the entire query result as interface with the given query rows
+func rows2Interfaces(rows *sql.Rows) (resultsSlice []map[string]interface{}, err error) {
+	fields, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		result, err := row2mapInterface(rows, fields)
+		if err != nil {
+			return nil, err
+		}
+		resultsSlice = append(resultsSlice, result)
+	}
+	return resultsSlice, nil
+}
+
+// QueryInterface return the query result as interface
+func (scanner *Scanner) QueryInterface(sqlStr string, args ...interface{}) ([]map[string]interface{}, error) {
+	var rows *sql.Rows
+	var err error
+	if len(args) > 0 {
+		rows, err = scanner.Query(sqlStr, args)
+	} else {
+		rows, err = scanner.Query(sqlStr)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return rows2Interfaces(rows)
+}
+
+func (scanner *Scanner) QueryJson(sqlStr string, args ...interface{}) (*json.RawMessage, error) {
+	var jsonData *json.RawMessage
+	var row *sql.Row
+	if len(args) > 0 {
+		row = scanner.QueryRow(sqlStr, args)
+	} else {
+		row = scanner.QueryRow(sqlStr)
+	}
+	err := row.Scan(&jsonData)
+	if err != nil {
+		return nil, err
+	}
+	return jsonData, nil
+}
